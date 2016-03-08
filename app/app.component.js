90708af3d2926a028d40346471d90e63b9d45a12
aarskin <mwiemer@outlook.com>
2016-03-08 17:44:52 -0600

    template with meta tag for form redirect

--- app/app.component.js
+++ app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'vertikal-ninja',
-      template: '<h1>[Template] Hello Worlds!</h1>'
+      template: '<META http-equiv="refresh" content="0;URL=https://docs.google.com/forms/d/1Nao-ruKLp6ga7xCM1wow-3jOXb8Ui5JdgClL47nOUZo/viewform">'
     })
     .Class({
       constructor: function() {}
