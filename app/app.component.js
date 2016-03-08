e4fd825008a57d9dbc23b53d3f53f8dcb5bb6e74
aarskin <mwiemer@outlook.com>
2016-03-08 13:44:45 -0600

    add sytlesheet

--- app/app.component.js
+++ app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'vertikal.ninja',
-      template: '<h1>[APP] Hello Worlds!</h1>'
+      template: '<h1>[Template] Hello Worlds!</h1>'
     })
     .Class({
       constructor: function() {}
